676e4c8b16b563512bfa8ab4beb2d11a90be6336
Xynon Marai <Xynon7@users.noreply.github.com>
2021-02-03 13:52:04 -0600

    Adding Class Skeletons for GC,PP, and SH

---
+++ Froglet-Islet/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Author: Chase Clawson
+ * Date Created: 02/03/2021
+ * Last Modified Date: 02/03/2021
+ * Last Modified By: 02/03/2021
+ * Description: Wrapper over Unity's SceneManager to allow easy transitions
+ */
+
+
+public class SceneHandler : MonoBehaviour
+{
+    public enum Areas
+    {
+        MainMenu,
+        Tutorial
+    }
+
+    public void LoadArea(Areas area)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ReturnToMainMenu()
+    {
+        throw new NotImplementedException();
+    }
+}
