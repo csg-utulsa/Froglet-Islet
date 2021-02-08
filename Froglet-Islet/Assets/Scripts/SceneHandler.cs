d3712e0cebb6ca794db59c0974994d4a6941a532
Xynon Marai <Xynon7@users.noreply.github.com>
2021-02-08 12:54:57 -0600

    Making SceneHandler Static and Adding functionality to area loading

--- Froglet-Islet/Assets/Scripts/SceneHandler.cs
+++ Froglet-Islet/Assets/Scripts/SceneHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 /*
@@ -13,21 +14,30 @@ using UnityEngine;
  */
 
 
-public class SceneHandler : MonoBehaviour
+public static class SceneHandler
 {
+    //Publicly accessable enum of the loadable areas in the game, names here may be different from scene names
     public enum Areas
     {
         MainMenu,
         Tutorial
     }
 
-    public void LoadArea(Areas area)
+    //Private list of variables to convert from the enum to a string for loading. allows us to change scene names here without having to worry about breaking other parts of the project
+    private static Dictionary<Areas, string> sceneNames = new Dictionary<Areas, string>() { { Areas.MainMenu, "MainMenu" }, { Areas.Tutorial, "Tutorial"} };
+
+    //This method will take an enum and use Unity's SceneManager to swap scenes. Any loading, unloading, and more complex scene handling will be done here if needed.
+    public static void LoadArea(Areas area)
     {
-        throw new NotImplementedException();
+        SceneManager.LoadScene(sceneNames[area]);
     }
 
-    public void ReturnToMainMenu()
+    //This method will return to the main menu while clearing variable data to allow saving and loading without variables being carried over.
+    public static void ReturnToMainMenu()
     {
-        throw new NotImplementedException();
+        //TODO: Level variable clearing
+        //TODO: Player variable clearing
+        //TODO: Game control variable clearing
+        SceneManager.LoadScene(sceneNames[Areas.MainMenu]);
     }
 }
