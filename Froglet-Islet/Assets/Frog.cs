bf0ff6d7fd7cab95d9f980899414eafaebfc1341
JimothyMarmalade <logan.edmund@gmail.com>
2021-03-03 13:19:50 -0600

    Hoo Boy
    All that's important from this commit is the basis of FrogData, so let's hope this doesn't break everything

--- Froglet-Islet/Assets/Frog.cs
+++ Froglet-Islet/Assets/Frog.cs
@@ -8,20 +8,7 @@ public class Frog : ScriptableObject
     //Assigned to Logan Edmund
     //Last updated 2/8/21
 
-    //Standard name for the Frog - what the player will see during normal gameplay
-    public string frogName;
-    //Species handles the specific offshoot of frog
-    public string frogSpecies;
-    //Genus handles the overall frog type -- handles what model and behaviors the frog will have
-    public string frogGenus;
-    //FrogID is a numeric value that can give all frogs a quick reference from the game controller
-    public int frogID;
-    //frogMaterial is a material assigned that will give the frog it's unique texture
-    public Material frogMaterial;
-
-    //frogModel keeps track of which model (and possibly animation sets) will be used for the frog
-    public GameObject frogModel;
-
+    public FrogData frogData;
     //Keeps track of wether or not the frog species has been collected by the player
     public int frogCollected;
 
---
+++ Froglet-Islet/Assets/FrogData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FrogData
+{
+    //Standard name for the Frog - what the player will see during normal gameplay
+    public string frogName;
+    //Species handles the specific offshoot of frog
+    public string frogSpecies;
+    //Genus handles the overall frog type -- handles what model and behaviors the frog will have
+    public string frogGenus;
+    //FrogID is a numeric value that can give all frogs a quick reference from the game controller
+    public int frogID;
+    //frogMaterial is a material assigned that will give the frog it's unique texture
+    public Material frogMaterial;
+    //frogModel keeps track of which model (and possibly animation sets) will be used for the frog
+    public GameObject frogModel;
+}
