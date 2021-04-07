dd244ef795526bea04cbe05c6b3ba1f3b67751f0
BaliOng <bemlo2000@gmail.com>
2021-04-07 12:34:41 -0500

    Adding intro

---
+++ Froglet-Islet/Assets/Scripts/IntroDialogue.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement; 
+
+public class IntroDialogue : MonoBehaviour
+{
+    public TMP_Text dialogBox;
+    int numClicks;
+    public AudioClip frogSounds;
+    public AudioSource sfxPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numClicks = 0;
+    }
+
+    public void buttonPress()
+    {
+        numClicks++;
+        switch(numClicks)
+        {
+            case 0:
+                break;
+            case 1:
+                dialogBox.text = "I guess I'm stuck on this island for now...";
+                break;
+            case 2:
+                dialogBox.text = "How am I going to get out of here!? What about my research?";
+                break;
+            case 3:
+                dialogBox.text = "Wait, what's that sound??";
+                sfxPlayer.PlayOneShot(frogSounds, 1f);
+                break;
+            case 4:
+                SceneManager.LoadScene("MainScene");
+                break;
+        }
+    }
+}
---
+++ Froglet-Islet/Assets/Scripts/MainAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.AudioSource;
+public class MainAudio : MonoBehaviour
+{
+    private static MainAudio instance = null;
+    private AudioSource audio;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        if (instance == this) return;
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
